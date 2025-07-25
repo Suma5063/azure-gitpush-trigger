5b4640919f13d3c9a1b487bba817c3c815a04ac5
Suma <sumah8050@gmail.com>
2025-07-25 11:27:02 +0530

    Fix: Renamed and corrected class name to SumaGitpushFunc

---
+++ src/main/java/com/SumaGitpushFunc.java
@@ -0,0 +1,37 @@
+package com;
+
+import java.util.Optional;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+public class SumaGitpushFunc {
+
+    @FunctionName("suma-gitpush-func")
+    public HttpResponseMessage run(
+        @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context) {
+        
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                          .body("Please pass a name on the query string or in the request body")
+                          .build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK)
+                          .body("Hello, " + name)
+                          .build();
+        }
+    }
+    
+}
